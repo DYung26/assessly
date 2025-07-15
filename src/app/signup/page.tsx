557c844a37d60a08baa0cc1e1a6c57919b239571
DYung26 <theadstore1407@gmail.com>
2025-07-15 15:59:14 +0100

    feat(auth): add `signup` page with `AuthLayout` and `SignupForm` integration

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,10 @@
+import AuthLayout from "@/components/AuthLayout";
+import SignupForm from "@/components/SignupForm";
+
+export default function Signup() {
+  return (
+    <AuthLayout>
+      <SignupForm />
+    </AuthLayout>
+  )
+}

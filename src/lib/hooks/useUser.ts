a2749a40accfe24b3a78008a82a08b209bb8b9db
DYung26 <theadstore1407@gmail.com>
2025-07-20 10:16:02 +0100

    feat(hooks): add `useUser` hook to fetch and manage authenticated user state via `React Query` and global store

---
+++ src/lib/hooks/useUser.ts
@@ -0,0 +1,48 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "@/lib/axiosInstance";
+import { useAuth, User } from "@/lib/store/auth";
+import { useEffect } from "react";
+
+export function useUser() {
+  const { accessToken, setUser } = useAuth();
+
+  const query = useQuery({
+    queryKey: ["me"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/user");
+      return res.data.data as User;
+    },
+    enabled: !!accessToken,
+    staleTime: Infinity,
+    retry: false,
+  });
+
+  const {
+    data,
+    isSuccess,
+    isError,
+    error,
+    isLoading,
+  } = query;
+
+  useEffect(() => {
+    if (isSuccess && data) {
+      console.log(data);
+      setUser(data);
+    }
+  }, [isSuccess, data]);
+
+  useEffect(() => {
+    if (isError) {
+      console.error("User fetch failed:", error);
+    }
+  }, [isError, error]);
+
+  useEffect(() => {
+    if (!isLoading) {
+      useAuth.setState({ loading: false });
+    }
+  }, [isLoading]);
+
+  return query;
+}

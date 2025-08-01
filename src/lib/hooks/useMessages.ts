de6f82634baac05aa55610389766600db2743d4e
DYung26 <theadstore1407@gmail.com>
2025-08-01 13:59:27 +0100

    feat(messages): add `message` fetching hook with `react-query` and store `sync` on success

---
+++ src/lib/hooks/useMessages.ts
@@ -0,0 +1,38 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "@/lib/axiosInstance";
+import { Message } from "@/types";
+import { useEffect } from "react";
+import { useMessageStore } from "../store/message";
+
+export function useMessages(chatId: string) {
+  const { setMessages } = useMessageStore();
+
+  const query = useQuery({
+    queryKey: ["messages", chatId],
+    queryFn: async () => {
+      const res = await axiosInstance.get(`/chat/${chatId}/messages`);
+      return res.data.data.messages as Message[];
+    },
+    enabled: !!chatId,
+  });
+
+  const {
+    data,
+    isSuccess,
+    /*isError,
+    error,
+    isLoading,*/
+  } = query;
+
+  useEffect(() => {
+    if (isSuccess && data) {
+      setMessages(chatId, data);
+      // useMessageStore.getState().setMessages(data);
+    }
+  }, [isSuccess, setMessages, data]);
+
+  return query;
+}
+

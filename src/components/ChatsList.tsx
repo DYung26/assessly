07f924f86c013d148b81f06a5ada84a06746a76d
DYung26 <theadstore1407@gmail.com>
2025-08-22 07:54:43 +0100

    feat(chat): add `ChatsList` component to display and navigate user chats

---
+++ src/components/ChatsList.tsx
@@ -0,0 +1,30 @@
+import { Chat } from "@/types";
+import { MessageCircleMore } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export default function ChatsList({ chats }: { chats?: Chat[] }) {
+  const router = useRouter();
+
+  if (!chats) return null;
+
+  return (
+    <div
+      className="max-w-3xl w-full p-2 justify-items-start flex flex-col gap-2"
+    >
+      <p className="font-semibold">Chats</p>
+      {chats.map((chat: Chat) => {
+        return (
+          <button
+            key={chat.id}
+            onClick={() => router.push(`/chat/${chat.id}`)}
+            className="flex items-center gap-2 pb-2 cursor-pointer border-b hover:bg-gray-100 transition-all duration-200 ease-in-out rounded-md p-2 text-left"
+          >
+            <MessageCircleMore size={18} />
+            <h2 className="text-sm ">{chat.title}</h2>
+            {/*<span className="text-xs text-gray-400">{chat.created_at}</span>*/}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
